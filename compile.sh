dd3435d4c7900de6d6de3a5c80af076ea0579488
Jee <dumping.runner332@addy.io>
2024-05-25 20:28:42 +0530

    Restructure

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@ if [[ "$1" =~ 'g' ]]; then
 fi
 
 # Sudo
-[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+# [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
 if [[ "$1" =~ 'v' ]]; then
     nix-store --repair --verify --check-contents
