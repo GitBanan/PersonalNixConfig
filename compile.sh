3a31fba048d2a8d0937423eebcae51c8b3099746
Jee <dumping.runner332@addy.io>
2024-05-25 20:25:04 +0530

    Restructure draft 4

--- compile.sh
+++ compile.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
 cd /home/jee/MainDirectory/PC/Linux/Nix/nix-config/
-# sudo nix flake update
-sudo nixos-rebuild switch --flake .#nixos --show-trace
-# sudo nixos-rebuild switch --update-input nixpkgs --flake .#nixos
-# sudo home-manager --flake .#nixos
 
-#sudo nix-store --repair --verify --check-contents
+if [[ "$1" =~ 'g' ]]; then
+    gacp "$2"
+fi
+
+# Sudo
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+
+if [[ "$1" =~ 'v' ]]; then
+    nix-store --repair --verify --check-contents
+fi
+
+if [[ "$1" =~ 'u' ]]; then
+    nix flake update
+fi
+
+nixos-rebuild switch --flake .#nixos --show-trace
+
+# home-manager --flake .#nixos
