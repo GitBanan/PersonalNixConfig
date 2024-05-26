b4c4417a96f251de5b4c862b934f1c294fa99a3a
Jee <dumping.runner332@addy.io>
2024-05-26 23:44:17 +0530

    Fixed

--- hosts/common/optional/sync.nix
+++ hosts/common/optional/sync.nix
@@ -15,57 +15,59 @@
     zrok
   ];
 
-  # Syncthing service
-  syncthing = {
-    enable = true;
-    user = "jee";
-    dataDir = "/home/jee/Sync";    # Default folder for new synced folders
-    configDir = "/home/jee/.config/syncthing";   # Folder for Syncthing's settings and keys
-    guiAddress = "127.0.0.1:8384";
-    openDefaultPorts = true;
+  services = {
+    # Syncthing service
+    syncthing = {
+      enable = true;
+      user = "jee";
+      dataDir = "/home/jee/Sync";    # Default folder for new synced folders
+      configDir = "/home/jee/.config/syncthing";   # Folder for Syncthing's settings and keys
+      guiAddress = "127.0.0.1:8384";
+      openDefaultPorts = true;
 
-    overrideDevices = true;
-    overrideFolders = true;
-    settings = {
-      devices = {
-        "Pixel7" = { id = "BEA64PF-MADNI3F-4ONKTOA-FMHRRAA-7IBPNBB-FGUYJUL-VSS7ON7-B62WYQC"; };
-      };
-      folders = {
-        "Aegis" = {
-          path = "/home/jee/Sync/Aegis";
-          devices = [ "Pixel7" ];
-          versioning = {
-            type = "trashcan";
-            params.cleanoutDays = "7";
-          };
+      overrideDevices = true;
+      overrideFolders = true;
+      settings = {
+        devices = {
+          "Pixel7" = { id = "BEA64PF-MADNI3F-4ONKTOA-FMHRRAA-7IBPNBB-FGUYJUL-VSS7ON7-B62WYQC"; };
         };
-        "DCIM" = {
-          path = "/home/jee/Sync/DCIM";
-          devices = [ "Pixel7" ];
-          versioning = {
-            type = "trashcan";
-            params = {
-              cleanoutDays = "7";
+        folders = {
+          "Aegis" = {
+            path = "/home/jee/Sync/Aegis";
+            devices = [ "Pixel7" ];
+            versioning = {
+              type = "trashcan";
+              params.cleanoutDays = "7";
             };
           };
-        };
-        "FileShare" = {
-          path = "/home/jee/Sync/FileShare";
-          devices = [ "Pixel7" ];
-          versioning = {
-            type = "trashcan";
-            params = {
-              cleanoutDays = "7";
+          "DCIM" = {
+            path = "/home/jee/Sync/DCIM";
+            devices = [ "Pixel7" ];
+            versioning = {
+              type = "trashcan";
+              params = {
+                cleanoutDays = "7";
+              };
             };
           };
-        };
-        "SeedVault" = {
-          path = "/home/jee/Sync/SeedVault";
-          devices = [ "Pixel7" ];
-          versioning = {
-            type = "trashcan";
-            params = {
-              cleanoutDays = "7";
+          "FileShare" = {
+            path = "/home/jee/Sync/FileShare";
+            devices = [ "Pixel7" ];
+            versioning = {
+              type = "trashcan";
+              params = {
+                cleanoutDays = "7";
+              };
+            };
+          };
+          "SeedVault" = {
+            path = "/home/jee/Sync/SeedVault";
+            devices = [ "Pixel7" ];
+            versioning = {
+              type = "trashcan";
+              params = {
+                cleanoutDays = "7";
+              };
             };
           };
         };
