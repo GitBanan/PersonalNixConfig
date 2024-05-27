05bc3a5e0b821b8767425c960af6356b8cf79fb8
Jee <dumping.runner332@addy.io>
2024-05-28 00:58:11 +0530

    GPU pass draft 2

--- hosts/common/optional/passthrough.nix
+++ hosts/common/optional/passthrough.nix
@@ -9,62 +9,27 @@
   # VIRSH_USB1 = "0000:00:14.0";
 
   startHook = ''
-    set -x
+    echo ''$VIRSH_GPU_VIDEO > "/sys/bus/pci/devices/''${VIRSH_GPU_VIDEO}/driver/unbind"
+    echo ''$VIRSH_GPU_AUDIO > "/sys/bus/pci/devices/''${VIRSH_GPU_AUDIO}/driver/unbind"
 
-    systemctl stop display-manager
-
-    echo 0 > /sys/class/vtconsole/vtcon0/bind
-    echo 0 > /sys/class/vtconsole/vtcon1/bind
-
-    #uncomment the next line if you're getting a black screen
-    echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
-
-    sleep 10
-
-    modprobe -r amdgpu
-    modprobe -r snd_hda_intel
-
-    virsh nodedev-detach ${VIRSH_GPU_VIDEO}
-    virsh nodedev-detach ${VIRSH_GPU_AUDIO}
-
-    sleep 10
-
-    modprobe vfio
-    modprobe vfio_pci
-    modprobe vfio_iommu_type1
+    sync
+    echo "3" > /proc/sys/vm/drop_caches
+    sync
+    echo "1" > /proc/sys/vm/compact_memory
   '';
 
   stopHook = ''
-    set -x
+    echo ''$VIRSH_GPU_VIDEO > "/sys/bus/pci/devices/''${VIRSH_GPU_VIDEO}/driver/unbind"
+    echo ''$VIRSH_GPU_AUDIO > "/sys/bus/pci/devices/''${VIRSH_GPU_AUDIO}/driver/unbind"
 
-    modprobe -r vfio
-    modprobe -r vfio_pci
-    modprobe -r vfio_iommu_type1
-
-    sleep 10
-
-    virsh nodedev-reattach ${VIRSH_GPU_VIDEO}
-    virsh nodedev-reattach ${VIRSH_GPU_AUDIO}
-
-    echo 1 > /sys/class/vtconsole/vtcon0/bind
-    echo 1 > /sys/class/vtconsole/vtcon1/bind
-
-    sleep 3
-
-    echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
-
-    modprobe amdgpu
-    modprobe snd_hda_intel
-
-    sleep 3
-
-    systemctl start display-manager
+    echo ''$VIRSH_GPU_VIDEO > /sys/bus/pci/drivers/amdgpu/bind
+    echo ''$VIRSH_GPU_AUDIO > /sys/bus/pci/drivers/snd_hda_intel/bind
   '';
 in {
   boot = {
     # kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = [ "intel_iommu=on" "iommu=pt" ];
-    kernelModules = [ "kvm-intel" "vfio" "vfio_iommu_type1" "vfio-pci" ];
+    kernelModules = [ "kvm-intel" "amdgpu" "vfio" "vfio_iommu_type1" "vfio-pci" ];
 
     # CHANGE: Don't forget to put your own PCI IDs here
     # extraModprobeConfig ="options vfio-pci ids=1002:73df,1002:ab28";
