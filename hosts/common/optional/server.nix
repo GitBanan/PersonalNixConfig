f51eb031652a6027167322ea0947e969736d58ea
Jee <dumping.runner332@addy.io>
2024-05-31 18:47:47 +0530

    Added temporary service to restart jellyfin

--- hosts/common/optional/server.nix
+++ hosts/common/optional/server.nix
@@ -32,6 +32,28 @@
     };
   };
 
+  # Restart Jellyfin if it failes to launch (buggy - temporary fix)
+  systemd.services = {
+    restart-jellyfin = {
+      description = "Restart Jellyfin";
+      enable = true;
+      after = [ "jellyfin.service" ];
+      wantedBy = [ "multi-user.target" ]; # Starts on boot
+
+      serviceConfig = {
+        Type = "simple";
+        User = "jee";
+        Restart = "always";
+        # ExecStart = "sleep 10; systemctl restart jellyfin.service";
+      };
+
+      script = ''
+        sleep 10
+        systemctl restart jellyfin.service
+      '';
+    };
+  };
+
   services = {
     # Setup Arr stacks
     sonarr = {
