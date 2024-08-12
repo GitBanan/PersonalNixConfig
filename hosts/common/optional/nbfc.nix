94feb9ec2e69929ee12b9609adc3bf47af9537b2
GitBanan <dumping.runner332@addy.io>
2024-08-12 15:32:57 +0530

    disable NBFC service

--- hosts/common/optional/nbfc.nix
+++ hosts/common/optional/nbfc.nix
@@ -12,8 +12,9 @@ in {
   environment.systemPackages = with pkgs; [
     inputs.nbfc-linux.packages.x86_64-linux.default
   ];
+
   systemd.services.nbfc_service = {
-    enable = true;
+    enable = false;
     description = "NoteBook FanControl service";
     serviceConfig.Type = "simple";
     path = [pkgs.kmod];
