8bd375358a0144137cc8fe70a887fc57dab4aa97
GitBanan <dumping.runner332@addy.io>
2025-04-25 22:50:56 +0530

    Removed gfp

--- hosts/common/global/alias.nix
+++ hosts/common/global/alias.nix
@@ -1,6 +1,5 @@
 {
   environment.shellAliases = {
     gacp = '' function _gacp(){ git add .; git commit -am "$1"; git push; };_gacp '';
-    gfp = '' function _gacp(){ git fetch; git pull; };_gacp '';
   };
 }
