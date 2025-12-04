b22b0629bb51c271de8bc815c5be4e34754843a1
GitBanan <github@frostyhill.top>
2025-12-05 00:10:57 +0530

    Fixed SSH keys, flatpak service and disabled jellyfin client

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -21,11 +21,10 @@
     # content of authorized_keys file
     # note: ssh-copy-id will add user@your-machine after the public key
     # but we can remove the "@your-machine" part
-    ''
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIENDwtYhKezoBJ9Tpe2+G/aw2wxN9SYJdIlDoRXDiCYV Pixel 7
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINB6xtWy/537P1pbQy5DYm36MYozEz4mSXPXKUsa4WUK deck@steamdeck
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINnLhtCqTkxR2iOJ+CukmSyG6f+cvC0i46yPXyMo+nLS jee@nano
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAFYkt9pfH/3jPilDkpPe6cjWjK4PkAY72DsbKdRj1Uq jee@hp260g9
-    ''
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIENDwtYhKezoBJ9Tpe2+G/aw2wxN9SYJdIlDoRXDiCYV Pixel 7"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINB6xtWy/537P1pbQy5DYm36MYozEz4mSXPXKUsa4WUK deck@steamdeck"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINnLhtCqTkxR2iOJ+CukmSyG6f+cvC0i46yPXyMo+nLS jee@nano"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAFYkt9pfH/3jPilDkpPe6cjWjK4PkAY72DsbKdRj1Uq jee@hp260g9"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPE8DBiLGdkGVsb/xGphb4eRxNnTknRYdeMBh071n/27 jee@vostro"
   ];
 }
--- hosts/common/optional/jellyfin-client.nix
+++ hosts/common/optional/jellyfin-client.nix
@@ -7,7 +7,6 @@
   environment.systemPackages = with pkgs; [
     # jellyfin-media-player
     # nur.repos.mio.jellyfin-media-player
-    nur.repos.mic92.hello-nur
     mpvScripts.mpris # Media keys support for jellyfin
     # Create symlink to script eg:
     # ln -s /nix/store/7g12vr81p0lc7f7q3ag8ww0z92vxvpqj-mpv-mpris-1.1/share/mpv/scripts/mpris.so /home/jee/.local/share/jellyfinmediaplayer/scripts/
