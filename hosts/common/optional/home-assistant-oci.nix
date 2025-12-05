3aca7392889f4df9ca136197ce856b00b8e0ab60
GitBanan <github@frostyhill.top>
2025-12-05 17:42:30 +0530

    Added Frigate, yet to test

--- hosts/common/optional/home-assistant-oci.nix
+++ hosts/common/optional/home-assistant-oci.nix
@@ -6,22 +6,73 @@
 }: {  
   virtualisation.oci-containers = {
     backend = "podman";
-    containers.homeassistant = {
-      volumes = [
-        "home-assistant:/config"
-        "/mnt/hdd_4tb_cctv/hass:/media"
-      ];
-      environment.TZ = "Europe/Berlin";
-      # Note: The image will not be updated on rebuilds, unless the version label changes
-      image = "ghcr.io/home-assistant/home-assistant:stable";
-      extraOptions = [ 
-        # Use the host network namespace for all sockets
-        "--network=host"
-        # Pass devices into the container, so Home Assistant can discover and make use of them
-        # "--device=/dev/ttyACM0:/dev/ttyACM0"
-      ];
+
+    containers = {
+      homeassistant = {
+        # Note: The image will not be updated on rebuilds, unless the version label changes
+        image = "ghcr.io/home-assistant/home-assistant:stable";
+        volumes = [
+          "home-assistant:/config"
+          "/mnt/hdd_4tb_cctv/hass:/media"
+          "/etc/localtime:/etc/localtime:ro"
+        ];
+        # environment.TZ = "Europe/Berlin";
+        extraOptions = [
+          # Use the host network namespace for all sockets
+          "--network=host"
+
+          # Hardware acceleration (Intel/AMD/Nvidia fallback)
+          "--device=/dev/dri"
+
+          # Pass devices into the container, so Home Assistant can discover and make use of them
+          # "--device=/dev/ttyACM0:/dev/ttyACM0"
+        ];
+      };
+
+      frigate = {
+        # autoStart = true;
+        # privileged = true;
+        # shmSize = "674m";
+        image = "ghcr.io/blakeblackshear/frigate:stable";
+        volumes = [
+          "frigate:/config"
+          "/mnt/hdd_4tb_cctv/hass:/media"
+          "/etc/localtime:/etc/localtime:ro"
+        ];
+        ports = [
+            "8971:8971"
+            "5000:5000" # Internal unauthenticated access. Expose carefully.
+            "8554:8554" # RTSP feeds
+            "8555:8555/tcp" # WebRTC over tcp
+            "8555:8555/udp" # WebRTC over udp
+        ];
+        extraOptions = [
+          # Use the host network namespace for all sockets
+          # "--network=host"
+
+          # Hardware acceleration (Intel/AMD/Nvidia fallback)
+          "--device=/dev/dri"
+
+          # Pass devices into the container, so Home Assistant can discover and make use of them
+          # "--device=/dev/ttyACM0:/dev/ttyACM0"
+        ];
+      };
     };
   };
+
+  services.mosquitto = {
+    enable = true;
+    listeners = [
+      {
+        acl = [ "pattern readwrite #" ];
+        omitPasswordAuth = true;
+        settings.allow_anonymous = true;
+      }
+    ];
+  };
   
-  networking.firewall.allowedTCPPorts = [ 8123 ];
+  networking.firewall.allowedTCPPorts = [
+    8123  # HASS
+    1883  # MQTT
+  ];
 }
